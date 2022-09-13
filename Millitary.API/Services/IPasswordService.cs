41b1754fb84f11719b39b54ddfa883fb200499ed
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:29:10 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Services/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace RudoRwedu.API.Services
+{
+    public interface IPasswordService
+    {
+        string HashPassword(string password);
+        bool VerifyHash(string password, string hashedPassword);
+    }
+}
