1e845df7e3931d6f556603c358cff0b548feb982
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:27:15 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Local/UpdateCompanyRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace RudoRwedu.API.Models.Local
+{
+    public class UpdateCompanyRequest
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
