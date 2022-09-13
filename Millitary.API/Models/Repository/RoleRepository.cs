f29f1345e62e53eeffe54597180c8a5d3e4870c1
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:28:29 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Repository/RoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using RudoRwedu.API.Models.Data;
+using RudoRwedu.API.Models.Repository.IRepository;
+
+namespace RudoRwedu.API.Models.Repository
+{
+    public class RoleRepository : Repository<Role>, IRoleRepository
+    {
+        private readonly AppDbContext _context;
+
+        public RoleRepository(AppDbContext context) : base(context) => _context = context;
+    }
+}
