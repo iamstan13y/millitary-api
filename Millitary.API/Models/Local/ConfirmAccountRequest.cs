fdbbe0a3e82fccbf36160b54ef7b0908a915b98e
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:26:32 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Local/ConfirmAccountRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RudoRwedu.API.Models.Local
+{
+    public class ConfirmAccountRequest
+    {
+        public string? Email { get; set; }
+        public string? ConfirmationCode { get; set; }
+    }
+}
