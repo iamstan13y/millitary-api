25e5cf90c0a9814fc1e0876dcaa8721f4580145d
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:25:54 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RudoRwedu.API.Models.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Role>? Roles { get; set; }
+        public DbSet<Account>? Accounts { get; set; }
+        public DbSet<GeneratedCode>? GeneratedCodes { get; set; }
+        public DbSet<Category>? Category { get; set; }
+        public DbSet<Company>? Companies { get; set; }
+    }
+}
