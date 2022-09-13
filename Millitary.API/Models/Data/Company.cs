15c91ececd38b97fab99361d19cf178d1d070b95
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:26:04 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Data/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace RudoRwedu.API.Models.Data
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public Account? Account { get; set; }
+    }
+}
