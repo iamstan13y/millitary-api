81fedf8c30c351648aa81671b88633168daaa92a
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:26:48 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Local/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RudoRwedu.API.Models.Local
+{
+    public class ResetPasswordRequest
+    {
+        public string? UserEmail { get; set; }
+        public string? VerificationCode { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
