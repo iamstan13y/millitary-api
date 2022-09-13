4b85a349fdbd317a09c3536f5b92ba6d548ec9bc
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:28:03 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Repository/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using RudoRwedu.API.Models.Data;
+using RudoRwedu.API.Models.Repository.IRepository;
+
+namespace RudoRwedu.API.Models.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context) : base(context) => _context = context;
+    }
+}
