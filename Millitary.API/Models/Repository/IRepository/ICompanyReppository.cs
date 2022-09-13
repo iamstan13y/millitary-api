f8e9d3e7abc25b886bedf7e4410a61ec43ea336b
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:27:33 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Repository/IRepository/ICompanyReppository.cs
@@ -0,0 +1,11 @@
+﻿using RudoRwedu.API.Models.Data;
+using RudoRwedu.API.Models.Local;
+
+namespace RudoRwedu.API.Models.Repository.IRepository
+{
+    public interface ICompanyRepository : IRepository<Company>
+    {
+        Task<Result<Company>> GetByAccountId(int accountId);
+        Task<Result<IEnumerable<Company>>> GetByCategoryIdAsync(int categoryId);
+    }
+}
