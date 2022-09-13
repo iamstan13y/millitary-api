52e22631b5afe0ded39d982242b003614d090d6c
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:27:52 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace RudoRwedu.API.Models.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        IRoleRepository Role { get; }
+        ICompanyRepository Company { get; }
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+    }
+}
