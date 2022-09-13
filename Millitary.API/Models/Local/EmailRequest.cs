b4c831559a6171879fbc4021963399db335b6522
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:26:36 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Local/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RudoRwedu.API.Models.Data
+{
+    public class EmailRequest
+    {
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
