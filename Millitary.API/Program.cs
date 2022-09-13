1774df601960a5ec23d4aff3a65aa4cec5c7b389
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:34:50 +0200

    Production Settings.

--- Millitary.API/Program.cs
+++ Millitary.API/Program.cs
@@ -70,7 +70,7 @@ builder.Services.AddAuthentication(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
