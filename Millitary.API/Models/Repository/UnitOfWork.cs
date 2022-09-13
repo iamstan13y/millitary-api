e3226ba026c3973f3a43168602e63b3643353c5e
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:28:33 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Repository/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using RudoRwedu.API.Models.Data;
+using RudoRwedu.API.Models.Repository.IRepository;
+
+namespace RudoRwedu.API.Models.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public IRoleRepository Role { get; private set; }
+        public ICompanyRepository Company { get; private set; }
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            Role = new RoleRepository(context);
+            Company = new CompanyRepository(context);
+            Category = new CategoryRepository(context);
+            _context = context;
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+    }
+}
