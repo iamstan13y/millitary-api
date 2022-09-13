f0d501c77b7e3c4ef494c8efadedda4ec587e65e
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:27:01 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Local/RoleRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace rudo_rwedu_api.Models.Local
+{
+    public class RoleRequest
+    {
+        public string? Name { get; set; }
+    }
+
+    public class UpdateRoleRequest : RoleRequest
+    {
+        public int Id { get; set; }
+    }
+}
