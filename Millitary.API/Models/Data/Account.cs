7e912b4c1edd1ca83a409e500f0773c997cf1414
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:25:50 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Data/Account.cs
@@ -0,0 +1,20 @@
+﻿using RudoRwedu.API.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RudoRwedu.API.Models.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public int RoleId { get; set; }
+        public AccountStatus Status { get; set; }
+        [ForeignKey("RoleId")]
+        public Role? Role { get; set; }
+        public DateTime DateCreated { get; set; }
+        [NotMapped]
+        public string? Token { get; set; }
+    }
+}
