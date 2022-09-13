e530b44fc773304eff662a64172b09e1d8bfdb0f
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:28:24 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Models/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RudoRwedu.API.Models.Data;
+using RudoRwedu.API.Models.Local;
+using RudoRwedu.API.Models.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace RudoRwedu.API.Models.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        internal DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async virtual Task<Result<T>> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return new Result<T>(entity);
+        }
+
+        public Task<Result<bool>> DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            return Task.FromResult(new Result<bool>(true));
+        }
+
+        public async Task<Result<T>> FindAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            return new Result<T>(entity);
+        }
+
+        public async Task<Result<IEnumerable<T>>> GetAllAsync()
+        {
+            var entities = await _dbSet.ToListAsync();
+            return new Result<IEnumerable<T>>(entities);
+        }
+
+        public async Task<Result<T>> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter)
+        {
+            var entity = await _dbSet.Where(filter).FirstOrDefaultAsync();
+            
+            return new Result<T>(entity);
+        }
+
+        public Task<Result<T>> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
