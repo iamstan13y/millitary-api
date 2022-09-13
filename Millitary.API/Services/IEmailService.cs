8661dd45ad86a497793a7f6cf7666891370ab8af
Keith Stanley <iamstan13y@gmail.com>
2022-09-13 09:28:55 +0200

    Initialized Millitary.API

---
+++ Millitary.API/Services/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using RudoRwedu.API.Models.Data;
+using RudoRwedu.API.Models.Local;
+
+namespace RudoRwedu.API.Services
+{
+    public interface IEmailService
+    {
+        Task<Result<string>> SendEmailAsync(EmailRequest email);
+    }
+}
