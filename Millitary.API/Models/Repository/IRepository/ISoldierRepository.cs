e1e0b5a1a96ab3a1e7ed28116bd20663f896f299
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:36:57 +0200

    Millitary.API - Setup I.

---
+++ Millitary.API/Models/Repository/IRepository/ISoldierRepository.cs
@@ -0,0 +1,10 @@
+﻿using Millitary.API.Models.Data;
+using Millitary.API.Models.Local;
+
+namespace Millitary.API.Models.Repository.IRepository
+{
+    public interface ICompanyRepository : IRepository<Soldier>
+    {
+        Task<Result<Soldier>> GetByAccountId(int accountId);
+    }
+}
