1acb7961fd046395e7fc0ec6e70303b4beeeed24
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:34:32 +0200

    Millitary.API - Setup I.

--- Millitary.API/Models/Data/AppDbContext.cs
+++ Millitary.API/Models/Data/AppDbContext.cs
@@ -12,7 +12,8 @@ namespace Millitary.API.Models.Data
         public DbSet<Role>? Roles { get; set; }
         public DbSet<Account>? Accounts { get; set; }
         public DbSet<GeneratedCode>? GeneratedCodes { get; set; }
-        public DbSet<Category>? Category { get; set; }
-        public DbSet<Company>? Companies { get; set; }
+        public DbSet<AmmoDispatch>? AmmoDispatches { get; set; }
+        public DbSet<Ammunition>? Ammunitions { get; set; }
+        public DbSet<Soldier>? Soldiers { get; set; }
     }
 }
