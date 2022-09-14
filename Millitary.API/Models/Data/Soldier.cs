5dca5af1a915eb342dd670d47c10c2148e7045ac
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:34:42 +0200

    Millitary.API - Setup I.

---
+++ Millitary.API/Models/Data/Soldier.cs
@@ -0,0 +1,15 @@
+﻿using Millitary.API.Models.Data;
+
+namespace Millitary.API.Models.Data
+{
+    public class Soldier
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? NationalId { get; set; }
+        public string? ForceId { get; set; }
+        public Account? Account { get; set; }
+    }
+}
