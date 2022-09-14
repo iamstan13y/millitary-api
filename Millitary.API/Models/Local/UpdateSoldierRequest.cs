0dd0a23b8cd836a0a8f513d412d6be2deb7d2a40
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:35:44 +0200

    Millitary.API - Setup I.

---
+++ Millitary.API/Models/Local/UpdateSoldierRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Millitary.API.Models.Local
+{
+    public class UpdateSoldierRequest
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? NationalId { get; set; }
+        public string? ForceId { get; set; }
+    }
+}
