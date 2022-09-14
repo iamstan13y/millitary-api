6ba924ffdbb72afc5c24f8adcb3a226ae035a845
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:34:27 +0200

    Millitary.API - Setup I.

---
+++ Millitary.API/Models/Data/Ammunition.cs
@@ -0,0 +1,11 @@
+﻿namespace Millitary.API.Models.Data
+{
+    public class Ammunition
+    {
+        public int Id { get; set; }
+        public string? SerialNumber { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
