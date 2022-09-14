e1152d1f1a41c1500fd24af58f5fb8f4d865f158
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:34:22 +0200

    Millitary.API - Setup I.

---
+++ Millitary.API/Models/Data/AmmoDispatch.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Millitary.API.Models.Data
+{
+    public class AmmoDispatch
+    {
+        public int Id { get; set; }
+        public int SoldierId { get; set; }
+        public int AmmunitionId { get; set; }
+        public string? Reason { get; set; }
+        public DateTime DateTaken { get; set; }
+        public DateTime DateReturned { get; set; }
+        public bool Status { get; set; } = false;
+        public int AuthorisedById { get; set; }
+        public Soldier? Soldier { get; set; }
+        public Ammunition? Ammunition { get; set; }
+        [ForeignKey("AuthorisedById")]
+        public Soldier? AuthorisedBy { get; set; }
+    }
+}
