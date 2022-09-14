cd47ec57b8254a82dfda7c74e9646c0ce215b4a8
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:34:48 +0200

    Millitary.API - Setup I.

--- Millitary.API/Models/Local/AccountRequest.cs
+++ Millitary.API/Models/Local/AccountRequest.cs
@@ -1,12 +1,11 @@
-﻿namespace RudoRwedu.API.Models.Local
+﻿namespace Millitary.API.Models.Local
 {
     public class AccountRequest
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? CompanyName { get; set; }
-        public int CategoryId { get; set; }
-        public string? Location { get; set; }
+        public string? NationalId { get; set; }
+        public string? ForceId { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Password { get; set; }
