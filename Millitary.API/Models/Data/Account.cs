a1d1560827d341566db3dc2305ec7a3fc26e98dd
Keith Stanley <iamstan13y@gmail.com>
2022-09-14 10:34:18 +0200

    Millitary.API - Setup I.

--- Millitary.API/Models/Data/Account.cs
+++ Millitary.API/Models/Data/Account.cs
@@ -9,8 +9,8 @@ namespace Millitary.API.Models.Data
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Password { get; set; }
+        public AccountStatus AccountStatus { get; set; }
         public int RoleId { get; set; }
-        public AccountStatus Status { get; set; }
         [ForeignKey("RoleId")]
         public Role? Role { get; set; }
         public DateTime DateCreated { get; set; }
